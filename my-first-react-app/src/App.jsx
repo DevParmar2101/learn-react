4a776d1ce588901b7081ae1b1a7797b3a9b47add
Dev Parmar <devp2101@gmail.com>
2026-07-13 15:48:50 +0530

    - create profile card and added it in App.jsx file

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './components/Header';
+import ProfileCard from "./components/ProfileCard.jsx";
+
 function App() {
   return (
       <div>
         <Header />
         <p>Welcome Dev</p>
+          <br/>
+          <ProfileCard />
       </div>
   );
 }
---
+++ my-first-react-app/src/components/ProfileCard.jsx
@@ -0,0 +1,11 @@
+function ProfileCard() {
+    return (
+        <div>
+            <h2>Profile Details</h2>
+            <span>Name: Dev parmar</span><br/>
+            <span>Position: Full stack developer</span><br/>
+            <span>Location: Ahmedabad</span>
+        </div>
+    )
+}
+export default ProfileCard;
