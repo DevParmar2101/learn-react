424835407239c524bd47f52b89c03fa090fb7667
Dev Parmar <devp2101@gmail.com>
2026-07-13 16:11:01 +0530

    - concept of variable is added in this
    - change the mange variable to one varialbe as object

--- my-first-react-app/src/components/UserInfo.jsx
+++ my-first-react-app/src/components/UserInfo.jsx
@@ -10,7 +10,7 @@ function UserInfo()
 
     return (
         <div>
-            <h1>User Info</h1>
+            <h2>User Info</h2>
             <p>Name: {user.firstName} {user.lastName}</p>
             <p>Age: {user.age}</p>
             <p>Phone: {user.phone}</p>
