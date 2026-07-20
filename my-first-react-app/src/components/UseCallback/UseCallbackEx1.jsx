070e69a9544f47764f3d66a2817d978d975c0757
Dev Parmar <devp2101@gmail.com>
2026-07-20 14:32:59 +0530

    - Basic example of useCallback. It memorizes a function so React can reuse the same function reference until its dependencies change

--- my-first-react-app/src/components/UseCallback/UseCallbackEx1.jsx
+++ my-first-react-app/src/components/UseCallback/UseCallbackEx1.jsx
@@ -8,6 +8,20 @@ function UseCallbackEx1() {
         console.log("Button Clicked!");
     }, []);
 
+    function increaseCount()
+    {
+        setCount(count + 1);
+    }
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={increaseCount}>Increase Count</button>
+            <br/> <br/>
+            <input type="text" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value) }/>
+            <br/> <br/>
+            <button onClick={showMessage}>Show message</button>
+        </div>
+    );
 
 }
 
--- my-first-react-app/src/pages/UseCallback.jsx
+++ my-first-react-app/src/pages/UseCallback.jsx
@@ -1,5 +1,15 @@
 import UseCallbackEx1 from "../components/UseCallback/UseCallbackEx1";
 function UseCallback() {
+    return (
+        <div>
+            <h2>UseCallback</h2>
+            <hr />
+
+            <h2>Exercise 1 (Basic example of useCallback. It memorizes a function so React can reuse the same function reference until its dependencies change.)</h2>
+            <UseCallbackEx1 />
+            <hr/>
+        </div>
+    );
 
 }
 export default UseCallback;
