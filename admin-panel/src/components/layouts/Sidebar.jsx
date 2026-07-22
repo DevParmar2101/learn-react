489d9d2daeaa187914982e066a411565449895af
Dev Parmar <devp2101@gmail.com>
2026-07-22 12:52:07 +0530

    - sidebar bug fix

--- admin-panel/src/components/layouts/Sidebar.jsx
+++ admin-panel/src/components/layouts/Sidebar.jsx
@@ -1,16 +1,21 @@
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+
 function Sidebar() {
+    const location = useLocation();
+
     return (
         <aside className="left-sidebar">
             <div>
                 <div className="brand-logo d-flex align-items-center justify-content-between">
-                    <Link className="text-nowrap logo-img" to="/">
-                        <img src="./src/assets/images/logos/dark-logo.svg" className="dark-logo" width="180" alt=""/>
-                    </Link>
+                    <NavLink className="text-nowrap logo-img" to="/">
+                        <img src="./src/assets/images/logos/dark-logo.svg" className="dark-logo" width="180" alt="" />
+                    </NavLink>
+
                     <div className="close-btn d-lg-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                         <i className="ti ti-x fs-8"></i>
                     </div>
                 </div>
+
                 <nav className="sidebar-nav scroll-sidebar" data-simplebar>
                     <ul id="sidebarnav">
                         <li className="nav-small-cap">
@@ -18,33 +23,32 @@ function Sidebar() {
                             <span className="hide-menu">Home</span>
                         </li>
 
-                        <li className="sidebar-item">
-                            <Link className="sidebar-link" aria-expanded="false" to="/">
+                        <li className={location.pathname === "/" ? "sidebar-item selected" : "sidebar-item"}>
+                            <NavLink to="/" end className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link" }>
                                 <span>
                                     <i className="ti ti-aperture"></i>
                                 </span>
                                 <span className="hide-menu">Dashboard</span>
-                            </Link>
+                            </NavLink>
                         </li>
 
-                        <li className="sidebar-item">
-                            <Link className="sidebar-link" aria-expanded="false" to="/datable">
+                        <li className={location.pathname === "/datable" ? "sidebar-item selected" : "sidebar-item"}>
+                            <NavLink to="/datable" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link" }>
                                 <span>
                                     <i className="ti ti-table"></i>
                                 </span>
                                 <span className="hide-menu">Datable</span>
-                            </Link>
+                            </NavLink>
                         </li>
 
-                        <li className="sidebar-item">
-                            <Link className="sidebar-link" aria-expanded="false" to="/form">
+                        <li className={location.pathname === "/form" ? "sidebar-item selected" : "sidebar-item"}>
+                            <NavLink to="/form" className={({ isActive }) => isActive ? "sidebar-link active" : "sidebar-link" }>
                                 <span>
                                     <i className="ti ti-forms"></i>
                                 </span>
                                 <span className="hide-menu">Forms</span>
-                            </Link>
+                            </NavLink>
                         </li>
-
                     </ul>
                 </nav>
             </div>
@@ -52,4 +56,4 @@ function Sidebar() {
     );
 }
 
-export default Sidebar
+export default Sidebar;
