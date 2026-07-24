edd8159ff4af3270560437791e7eb7f282cc2568
Dev Parmar <devp2101@gmail.com>
2026-07-24 17:42:09 +0530

    - enhancement and bug fixes

--- admin-panel/src/components/auth/partial/LoginForm.jsx
+++ admin-panel/src/components/auth/partial/LoginForm.jsx
@@ -3,8 +3,7 @@ function LoginForm() {
         <form>
             <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
-                <input type="email" className="form-control" id="exampleInputEmail1"
-                       aria-describedby="emailHelp"/>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
             </div>
             <div className="mb-4">
                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
@@ -12,20 +11,17 @@ function LoginForm() {
             </div>
             <div className="d-flex align-items-center justify-content-between mb-4">
                 <div className="form-check">
-                    <input className="form-check-input primary" type="checkbox" value=""
-                           id="flexCheckChecked" checked/>
+                    <input className="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked/>
                     <label className="form-check-label text-dark" htmlFor="flexCheckChecked">
-                        Remeber this Device
+                        Remember this Device
                     </label>
                 </div>
-                <a className="text-primary fw-medium" href="./authentication-forgot-password.html">Forgot
-                    Password ?</a>
+                <a className="text-primary fw-medium" href="./authentication-forgot-password.html">Forgot Password ?</a>
             </div>
             <a href="./index.html" className="btn btn-primary w-100 py-8 mb-4 rounded-2">Sign In</a>
             <div className="d-flex align-items-center justify-content-center">
                 <p className="fs-4 mb-0 fw-medium">New to Modernize?</p>
-                <a className="text-primary fw-medium ms-2" href="./authentication-register.html">Create
-                    an account</a>
+                <a className="text-primary fw-medium ms-2" href="./authentication-register.html">Create an account</a>
             </div>
         </form>
 
