5ff86aba0bf3752eb8a7d0aab8d42027cc81694d
Dev Parmar <devp2101@gmail.com>
2026-07-24 18:49:44 +0530

    - links send diynamic

--- admin-panel/src/components/auth/partial/ForgotPasswordForm.jsx
+++ admin-panel/src/components/auth/partial/ForgotPasswordForm.jsx
@@ -7,7 +7,8 @@ function ForgotPasswordForm() {
                 <label htmlFor="email" className="form-label">Email</label>
                 <input type="email" className="form-control" id="email" aria-describedby="usernameHelp"/>
             </div>
-            <a href="/login" className="btn btn-primary w-100 py-8 mb-4 rounded-2">Send</a>
+            <NavLink to="/forgot-password" className="btn btn-primary w-100 py-8 mb-4 rounded-2">Send</NavLink>
+
             <div className="d-flex align-items-center justify-content-center">
                 <p className="fs-4 mb-0 fw-medium">New to Modernize?</p>
                 <NavLink to="/register" className="text-primary fw-medium ms-2">Create an account</NavLink>
