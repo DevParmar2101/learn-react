673d13537240d891534f4030af6a28b78fa21268
Dev Parmar <devp2101@gmail.com>
2026-07-24 17:30:21 +0530

    - Auth layout design integrated

--- admin-panel/src/layouts/AuthLayout.jsx
+++ admin-panel/src/layouts/AuthLayout.jsx
@@ -2,12 +2,11 @@ import {Outlet} from "react-router-dom";
 
 function AuthLayout() {
     return (
-
         <div className="page-wrapper" id="main-wrapper" data-layout="vertical" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
             <div className="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
                 <Outlet/>
             </div>
         </div>
-    )
+    );
 }
 export default AuthLayout;
