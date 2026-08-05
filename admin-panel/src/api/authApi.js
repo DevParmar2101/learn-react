a099e0b2c63516d7cb1489604974474a3e0dfb9a
Dev Parmar <devp2101@gmail.com>
2026-08-05 20:00:07 +0530

    - sign in api start integration

--- admin-panel/src/api/authApi.js
+++ admin-panel/src/api/authApi.js
@@ -1,4 +1,5 @@
 import api from "./axios";
 export const login = (data) => api.post("/login", data);
+export const register = (data) => api.post("/register", data);
 export const logout = () => api.post("/logout");
 export const profile = () => api.get("/user-profile");
--- admin-panel/src/components/auth/partial/RegisterForm.jsx
+++ admin-panel/src/components/auth/partial/RegisterForm.jsx
@@ -1,19 +1,59 @@
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { register } from "../../../api/authApi";
+import {formatDate} from "../../../utils/helper.js";
 
 function RegisterForm() {
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setErrors({});
+
+        try{
+            const response = await register(formData);
+            console.log(response.data.data);
+
+            navigate('/');
+        } catch(error) {
+            if (error.response?.status === 422) {
+                setErrors(error.response.data.errors);
+            }else{
+                alert(error.response?.data?.message || "Registration Failed");
+            }
+        }finally {
+            setLoading(false);
+        }
+    };
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="fullname" className="form-label">FullName</label>
-                <input type="text" className="form-control" id="fullname" aria-describedby="emailHelp"/>
+                <input type="text" className="form-control" id="fullname" value={formData.name} onChange={handleChange}/>
             </div>
             <div className="mb-4">
                 <label htmlFor="email" className="form-label">Email</label>
-                <input type="email" className="form-control" id="email"/>
+                <input type="email" className="form-control" id="email" value={formData.email} onChange={handleChange}/>
             </div>
             <div className="mb-4">
                 <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control" id="password"/>
+                <input type="password" className="form-control" id="password" value={formData.password} onChange={handleChange}/>
             </div>
             <NavLink to="/register" className="btn btn-primary w-100 py-8 mb-4 rounded-2">Register</NavLink>
             <div className="d-flex align-items-center justify-content-center">
