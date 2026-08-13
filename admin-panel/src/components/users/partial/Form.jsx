65ac08a319acd8cfcbbb3ab0db9b6ca16f021eb7
Dev Parmar <devp2101@gmail.com>
2026-08-13 19:59:04 +0530

    - bug fix

--- admin-panel/src/components/users/partial/Form.jsx
+++ admin-panel/src/components/users/partial/Form.jsx
@@ -1,16 +1,19 @@
 function UserForm() {
     return (
         <div className="card-body p-4 border-bottom">
+
             <div className="row">
                 <div className="col-lg-6 mb-4">
                     <label htmlFor="name" className="form-label fw-semibold">Username</label>
                     <input type="text" name="name" className="form-control" id="name"/>
                 </div>
+
                 <div className="col-lg-6 mb-4">
                     <label htmlFor="email" className="form-label fw-semibold">Email</label>
                     <input type="text" name="email" className="form-control"/>
                 </div>
             </div>
+
             <div className="row">
                 <div className="col-lg-6 mb-4">
                     <label htmlFor="password" className="form-label fw-semibold">Password</label>
@@ -24,16 +27,19 @@ function UserForm() {
                     {/*<span className="input-group-text bg-transparent px-6" id="basic-addon1"><i className="ti ti-eye fs-6"></i></span>*/}
                 </div>
             </div>
+
             <div className="row">
                 <div className="col-lg-6 mb-4">
                     <label htmlFor="phone" className="form-label fw-semibold">Phone Number</label>
                     <input type="text" name="phone" className="form-control" id="phone"/>
                 </div>
+
                 <div className="col-lg-6 mb-4">
                     <label htmlFor="status" className="form-label fw-semibold">Status</label>
                     <input type="text" name="status" className="form-control" id="status"/>
                 </div>
             </div>
+
         </div>
     );
 }
